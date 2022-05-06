ea25b74d09b20f3dec830c404289c54a8a7020f5
riskywisky1994 <veerjain1994@gmail.com>
2022-05-06 22:43:06 +0530

    Initial commit

---
+++ src/Bharat.java
@@ -0,0 +1,18 @@
+public class Bharat {
+
+
+    public static void main(String[] args){
+
+        System.out.println("i love my India");
+
+        System.out.println("life isi ka name hai");
+
+        System.out.println("living like a king");
+
+        System.out.println("i love my family");
+
+        System.out.println("violence likes me i can't avoid");
+
+    }
+
+}
