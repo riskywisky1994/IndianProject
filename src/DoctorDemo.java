0109d59f12342f05846af94ab2f79847f63ff1fc
riskywisky1994 <veerjain1994@gmail.com>
2022-05-08 15:05:26 +0530

    DoctorDemo

---
+++ src/DoctorDemo.java
@@ -0,0 +1,20 @@
+public class DoctorDemo {
+
+    public static void main(String[] args){
+
+        // SYNTAX : Datatype var_name=value;
+
+        String Doctor_name= "DR. Dharmesh Jain";
+        System.out.println("printing variables:"+Doctor_name);
+        String Specialization= "Physician";
+        System.out.println("printing specialization:" +Specialization);
+        int age=38;
+        System.out.println("printing age:" +age);
+        double timing=09.00 ;
+        System.out.println("printing timing:" +timing);
+        String Place= "Janakpuri,Delhi";
+        System.out.println("printing place:" +Place);
+
+
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+
+
+    public static void main(String[] args){
+
+        // SYNTAX : DATATYPE employee_name=value;
+
+        String employee_name= "Veer Jain";
+        System.out.println("printing variables" +employee_name);
+        String department= "Accounts";
+        System.out.println("printing department" +department);
+        int age=25;
+        System.out.println("printing age"+age);
+        int salary=500000;
+        System.out.println("printing salary"+salary);
+
+
+
+    }
+}
