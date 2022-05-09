4b734162ae2a439db737ba098e8b1dd36579647a
riskywisky1994 <veerjain1994@gmail.com>
2022-05-09 17:11:21 +0530

    conditional

---
+++ src/iforelse.java
@@ -0,0 +1,35 @@
+
+public class iforelse {
+
+    public static void main(String[] args){
+
+        //SYNTAX: if(condition){ } else{}
+
+        int age=17;
+        if (age>18){
+           System.out.println("person is eligible for voting");
+        }
+        else{
+            System.out.println("person is not eligible for voting");
+
+        }
+        String city="Delhi";
+        if (city=="Agra"){
+            System.out.println("person is in Agra");
+        }
+        else{
+            System.out.println("person is not in Agra");
+
+        }
+        double salary=34000.5;
+        if (salary>35000){
+            System.out.println("good salary");
+        }
+        else{
+            System.out.println("not good salary");
+
+        }
+
+    }
+
+}
---
+++ src/iforelse2.java
@@ -0,0 +1,35 @@
+public class iforelse2 {
+    public static void main(String[] args){
+
+        //SYNTAX: if(condition){} else{}
+
+        String mobile="iphone";
+        if (mobile=="android"){
+            System.out.println("mobile is android");
+        }
+        else{
+            System.out.println("mobile is not android");
+        }
+        String brand="vivo";
+        if (brand=="vivo"){
+            System.out.println("brand is vivo");
+        }
+        else {
+            System.out.println("brand is not vivo");
+        }
+        int price=25000;
+        if (price>24999){
+            System.out.println("price is good");
+        }
+        else{
+            System.out.println("price is not good");
+        }
+        int ram=8;
+        if (ram>4){
+            System.out.println("ram is good");
+        }
+        else{
+            System.out.println("ram is not good");
+        }
+    }
+}
